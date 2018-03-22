48cff010844c6481ed7181fe7702e9309f8716f6
zhuoqi-chen <zhuoqi_chen@126.com>
2018-03-22 22:58:14 +0800

    TodoAdd component test done

--- src/components/TodoAdd.test.js
+++ src/components/TodoAdd.test.js
@@ -2,8 +2,32 @@ import { shallow } from '@vue/test-utils';
 import TodoAdd from './TodoAdd';
 
 describe('TodoAdd', async () => {
-  it('created', () => {
-    const wapper = shallow(TodoAdd);
-    console.log(wapper.html());
+  describe('init', () => {
+    it('should behave a input', () => {
+      const wapper = shallow(TodoAdd);
+      expect(wapper.contains('input')).toBe(true);
+    });
+  });
+  describe('methods', () => {
+    describe('add', () => {
+      it('title is empty,it should return', () => {
+        const wapper = shallow(TodoAdd);
+        wapper.setData({
+          title: '',
+        });
+        wapper.vm.add();
+        expect(wapper.emitted().add).toBeFalsy();
+      });
+      it('title is not empty,it should emit "add" event', () => {
+        const wapper = shallow(TodoAdd);
+        wapper.setData({
+          title: 'xxx',
+        });
+        wapper.vm.add();
+        expect(wapper.emitted().add).toBeTruthy();
+        expect(wapper.emitted().add[0][0].title).toEqual('xxx');
+        expect(wapper.emitted().add[0][0].completed).toBe(false);
+      });
+    });
   });
 });
--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    TodoAdd
+    <input type="text" v-model="title">
   </div>
 </template>
 
 <script>
-// import { guid } from '@/js/Utils';
+import { guid } from '@/js/Utils';
 
 export default {
   name: 'TodoAdd',
+  data() {
+    return {
+      title: '',
+    };
+  },
+  methods: {
+    add() {
+      const title = this.title && this.title.trim();
+      if (!title) {
+        return;
+      }
+      const todo = {
+        title,
+        completed: false,
+        uuid: guid(),
+      };
+      this.$emit('add', todo);
+    },
+  },
 };
 </script>
 
